0441d2381163e6aea5dc33d467c9274dd63a47af
adrientaudiere <adrien.taudiere@zaclys.net>
2026-03-09 11:38:11 +0100

    Add gg_hill_lpq function enhancegg_bubbles_pq with list_phyloseq support and diff_cont comparisons

--- R/diff_pq.R
+++ R/diff_pq.R
@@ -1,11 +1,20 @@
 # to document
 
+#div_pq(data_fungi_mini,
+#   modality = "Height",
+#   indices = c("shannon", "simpson"),
+#   scales = c(0, 1, 2), 
+#   hill = TRUE)
+
+# If modality is null, then it will compute the indices for the whole dataset without grouping by any variable. This can be useful for getting an overall diversity estimate for the entire community.
+# If their is na in modality, treat the NA as a separate group and compute the indices for that group as well. This way, you can still get diversity estimates for samples with missing modality values without excluding them from the analysis.
+
 div_pq <- function(
   physeq,
-  modality,
+  modality = NULL,
   indices = "shannon",
   scales = NULL,
-  hill = FALSE,
+  hill = TRUE,
   aggregate = FALSE,
   funs = list(mean = mean, sd = sd)
 ) {
